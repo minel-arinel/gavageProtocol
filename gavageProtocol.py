3e54ceae7e28fea14ad647e8fa3c7d309d7fe13c
minel <minelarinel@gmail.com>
2022-10-24 09:25:59 -0400

    Creates experiment folder with metadata and injects stimulus

--- gavageProtocol.py
+++ gavageProtocol.py
@@ -4,20 +4,41 @@ from datetime import datetime as dt
 import time
 
 #### SET THE EXPERIMENT CONDITIONS ##
-saveInFolder = r'C:\Users\minel\iCloudDrive\Documents\Duke University\Naumann Lab\Gut-Brain\Gavage'
-genotype = 'elavl3GCaMP6s'
-age = 6 # in dpf
-feed = 'compunfed' # compunfed, unfed, or fed
-stimulus = 'fructose'
+data_folder = r'R:\data\minel\\'
+genotype = 'elavl3H2BGCaMP8m'
+age = 7 # in dpf
+stimulus = 'glucose'
 concentration = '500mM'
-fish_id = 10
+fish_id = 1
 
-startBaselineDuration = 10 # in seconds, duration between the start and injection
+region = 'vagal'
+side = 'R'
+
+startBaselineDuration = 180 # in seconds, duration between the start and injection
+
 ###
+exp_name = f'{genotype}_{age}dpf_{stimulus}_{concentration}_{fish_id}_{dt.today().strftime("%Y%m%d")}'
+exp_folder = os.path.join(data_folder, exp_name)
+os.mkdir(exp_folder)
 
-file = open(os.path.join(saveInFolder, f'{genotype}_{age}dpf_{feed}_{stimulus}_{concentration}_{fish_id}_\
-                                {dt.today().strftime("%Y%m%d")}.txt'), 'w')
-board = pyfirmata.Arduino('COM3')
+if region == 'vagal':
+    pregavage_folder = os.path.join(exp_folder, f'pregavage_{region}_{side}')
+else:
+    pregavage_folder = os.path.join(exp_folder, f'pregavage_{region}')
+os.mkdir(pregavage_folder)
+pregavage_anatomy_folder = os.path.join(pregavage_folder, os.path.basename(pregavage_folder)+'_anatomy')
+os.mkdir(pregavage_anatomy_folder)
+
+if region == 'vagal':
+    postgavage_folder = os.path.join(exp_folder, f'postgavage_{region}_{side}')
+else:
+    postgavage_folder = os.path.join(exp_folder, f'postgavage_{region}')
+os.mkdir(postgavage_folder)
+postgavage_anatomy_folder = os.path.join(postgavage_folder, os.path.basename(postgavage_folder)+'_anatomy')
+os.mkdir(postgavage_anatomy_folder)
+
+file = open(os.path.join(exp_folder, 'injection.txt'), 'w')
+board = pyfirmata.Arduino('COM9')
 led = board.get_pin('d:13:o')
 led.write(1)
 
